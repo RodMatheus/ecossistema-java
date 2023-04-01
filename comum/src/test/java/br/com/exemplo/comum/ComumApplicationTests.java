6ec5fdd4163737b2d71951f662c5d840f9dd55cf
Matheus Rodrigues <rodmatheus96@gmail.com>
2023-04-01 12:49:25 -0300

    Implementa configuração do Jacoco e Sonar (#28)

--- comum/src/test/java/br/com/exemplo/comum/ComumApplicationTests.java
+++ comum/src/test/java/br/com/exemplo/comum/ComumApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.exemplo.comum;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ComumApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
